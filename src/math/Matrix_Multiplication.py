9e273eeadeafce9ce645cf93465c7cad41422b28
RudraJad2005 <rudrajad2005@gmail.com>
2026-06-10 21:40:20 +0530

    Completed matrix multiplication from scratch with numpy

---
+++ src/math/Matrix_Multiplication.py
@@ -0,0 +1,20 @@
+matrix1 = [
+    [1,2,3], 
+    [4,5,6]
+]
+matrix2 = [
+    [7,8], 
+    [9,10], 
+    [11, 12]
+]
+
+result = [[0 for _ in range(len(matrix1[0]))] for _ in range(len(matrix2))]
+
+for i in range(len(matrix1)):
+    for j in range(len(matrix2[0])):
+        for k in range(len(matrix2)):
+
+            result[i][j] += matrix1[i][k] * matrix2[k][j]
+
+for row in result:
+    print(row)
