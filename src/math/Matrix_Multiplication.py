18393768a6731de12729a89db7f296f3d4fdc782
RudraJad2005 <rudrajad2005@gmail.com>
2026-06-11 20:29:05 +0530

    2 Vector's dot product from scratch with python

--- src/math/Matrix_Multiplication.py
+++ src/math/Matrix_Multiplication.py
@@ -20,3 +20,4 @@ for row in result:
     print(row)
 
     
+
--- src/math/vector_operations.py
+++ src/math/vector_operations.py
@@ -1,5 +1,4 @@
 
-
 print("Vector addition result:")
 vector1 = [1, 2, 3]
 
@@ -37,4 +36,18 @@ result = [0 for _ in range(len(vector1))]
 for i in range(len(vector1)):
 
     result[i] = vector1[i] - vector2[i]
+    print(result)
+
+
+
+print("Vector Dot Product")
+
+vector1 = [1, 2, 3]
+vector2 = [4, 5, 6]
+
+result = 0
+
+for i in range(len(vector1)):
+
+    result = result + vector1[i] * vector2[i]
     print(result)
